c3619cd6199214f71e042cd342ba54eea72946da
LQH1997 <2438090307@qq.com>
2018-06-14 19:14:49 +0800

    segmentation

--- HW5/15331159_EX5/src/main.cpp
+++ HW5/15331159_EX5/src/main.cpp
@@ -8,22 +8,14 @@ using namespace std;
 using namespace cimg_library;
 
 int main() {
-  cout << "aaa" << endl;
   string filePath;
   string head = "../dataset/";
-  cout << "aaa" << endl;
   ostu ostu1;
-  cout << "aaa" << endl;
   for(int i = 1; i <=100; i++) {
-    cout << "aaa" << endl;
     string temp = to_string(i);
-    cout << "aaa" << endl;
     temp = head+temp;
-    cout << "aaa" << endl;
     filePath = temp + ".JPG";
-    cout << "aaa" << endl;
     ostu ostu1(filePath);
-    cout << "aaa" << endl;
 }
 
   for(int i = 1; i <= 100; i++) {
